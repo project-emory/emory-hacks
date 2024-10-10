64a02499f02e7694c18349081e2964772dc404e0
Raphael Palacio <54969177+raphaelpalacio@users.noreply.github.com>
2024-10-10 18:34:44 -0400

    updated global css and homepage

--- emoryhacks2025/src/app/sections/homepage/homepage.jsx
+++ emoryhacks2025/src/app/sections/homepage/homepage.jsx
@@ -4,31 +4,39 @@ import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@n
 const HomePage = () => {
   return (
     <section className="homepage-bg">
-      {/* Navbar with shouldHideOnScroll and isBordered */}
-      <Navbar shouldHideOnScroll isBordered>
+      {/* Navbar with custom styling */}
+      <Navbar isBordered shouldHideOnScroll className="bg-transparent px-10">
         <NavbarBrand>
-          <p className="text-lg font-bold">Emory Hacks</p>
+          {/* Add your logo */}
+          <p className="text-2xl font-bold text-white">Emory Hacks</p>
         </NavbarBrand>
-        <NavbarContent>
+        
+        <NavbarContent className="hidden sm:flex gap-8" justify="center">
           <NavbarItem>
-            <Link href="#about">About</Link>
+            <Link href="#about" className="text-white text-lg">About</Link>
           </NavbarItem>
           <NavbarItem>
-            <Link href="#tracks">Tracks</Link>
+            <Link href="#tracks" className="text-white text-lg">Tracks</Link>
           </NavbarItem>
           <NavbarItem>
-            <Link href="#schedule">Schedule</Link>
+            <Link href="#schedule" className="text-white text-lg">Schedule</Link>
           </NavbarItem>
           <NavbarItem>
-            <Link href="#faq">FAQ</Link>
+            <Link href="#faq" className="text-white text-lg">FAQ</Link>
           </NavbarItem>
           <NavbarItem>
-            <Link href="#sponsors">Sponsors</Link>
+            <Link href="#sponsors" className="text-white text-lg">Sponsors</Link>
           </NavbarItem>
         </NavbarContent>
-        <NavbarItem>
-          <Button>Register</Button>
-        </NavbarItem>
+        
+        {/* "Apply" button styled as in the example */}
+        <NavbarContent justify="end">
+          <NavbarItem>
+            <Button as={Link} href="#" variant="flat" className="bg-pink-500 text-white hover:bg-pink-400">
+              Apply
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
       </Navbar>
 
       <div className="content">
