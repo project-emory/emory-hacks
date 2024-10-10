a462ac0074c6520d86c68b839e96e00dac86150f
Raphael Palacio <54969177+raphaelpalacio@users.noreply.github.com>
2024-10-10 16:18:06 -0400

    updated the page.jsx and tailwind config file

--- emoryhacks2025/tailwind.config.js
+++ emoryhacks2025/tailwind.config.js
@@ -4,12 +4,17 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,jsx,ts,tsx}', 
+    './public/**/*.html',
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      backgroundImage: {
+        'homepage': "url('/images/IMG_1023.png')",
       },
     },
   },
