0f5626a27529bdfe49ada6f83e7bc58802a7c4f9
Ethan Lee <ethan.lee@emory.edu>
2024-12-21 17:03:53 -0500

    Enhance SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
+import Head from "next/head";
 import SmoothScrolling from "./components/smoothScrolling";
 import "./globals.css";
-import { Raleway, Space_Grotesk } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -9,12 +10,32 @@ const spaceGrotesk = Space_Grotesk({
 
 export const metadata = {
   title: "Emory Hacks 2025",
-  description: "",
+  description:
+    "Join us for Emory Hacks 2025, a 36-hour hackathon at Emory University where innovation comes to life.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Emory Hacks 2025</title>
+        <meta
+          name="description"
+          content="Join us for Emory Hacks 2025, a 36-hour hackathon at Emory University where innovation comes to life."
+        />
+        <meta
+          name="keywords"
+          content="Emory Hacks, hackathon, Emory University, coding, programming, innovation"
+        />
+        <meta property="og:title" content="Emory Hacks 2025" />
+        <meta
+          property="og:description"
+          content="Join us for Emory Hacks 2025, a 36-hour hackathon at Emory University where innovation comes to life."
+        />
+        <meta property="og:image" content="../../public/logo.webp" />
+        <meta property="og:url" content="https://www.emoryhacks.com" />
+      </Head>
+
       <body className={`${spaceGrotesk.className} antialiased w-screen bg-bg`}>
         {/* <SmoothScrolling> */}
         {children}
