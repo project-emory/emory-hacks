b3b40a382c45c54ea7d3bad15a949390c2d44bfa
Ethan Lee <ethan.lee@emory.edu>
2025-02-05 15:15:16 -0500

    Refactor graphics.jsx to improve semantic structure by replacing h3 and h4 tags with p tags for better accessibility

--- src/app/sections/2-tracks/graphics.jsx
+++ src/app/sections/2-tracks/graphics.jsx
@@ -1,25 +1,26 @@
 "use client";
 
+import Parallax from "@/app/components/parallax";
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import { useRef, useState } from "react";
-import Parallax from "@/app/components/parallax";
+
 import {
-  bg,
   asteroid_1,
-  asteroid_2,
-  asteroid_3,
-  asteroid_4,
   asteroid_1_cropped,
+  asteroid_2,
   asteroid_2_cropped,
+  asteroid_3,
   asteroid_3_cropped,
+  asteroid_4,
   asteroid_4_cropped,
   asteroid_small_1,
   asteroid_small_2,
   asteroid_small_3,
   asteroid_small_4,
+  bg,
   planet,
 } from "./graphicsData";
-import { AnimatePresence, motion } from "framer-motion";
 
 const Graphics = () => {
   const container = useRef(null);
@@ -68,9 +69,9 @@ const Graphics = () => {
         >
           <Image src={asteroid_1_cropped} alt="asteroid" />
           <div className="absolute left-0 top-0 size-full flex items-center justify-center">
-            <h3 className="h-fit w-2/3 text-body text-center font-bold text-white">
+            <p className="h-fit w-2/3 text-body text-center font-bold text-white">
               Accessible Communication Technologies
-            </h3>
+            </p>
           </div>
           <AnimatePresence>
             {track === 0 && (
@@ -80,11 +81,11 @@ const Graphics = () => {
                 exit={{ opacity: 0 }}
                 className="absolute h-full w-[53vw] top-0 -left-[53vw] pl-[7vw] pr-[5vw] flex flex-col justify-center bg-gradient-to-r from-bg to-transparent"
               >
-                <h4 className="text-body-small text-white font-bold">
+                <p className="text-body-small text-white font-bold">
                   Develop tools that make communication easier for people with
                   disabilities, such as speech-to-text applications, sign
                   language translators, or eye-tracking communication devices.
-                </h4>
+                </p>
                 <ul className="hidden md:block mt-2 text-body-small text-white list-disc list-inside">
                   <li>Help disabled people</li>
                   <li>Make technology more accessible</li>
@@ -108,9 +109,9 @@ const Graphics = () => {
         >
           <Image src={asteroid_2_cropped} alt="asteroid" />
           <div className="absolute left-0 top-0 size-full flex items-center justify-center">
-            <h3 className="h-fit w-2/3 text-body text-center font-bold text-white">
+            <p className="h-fit w-2/3 text-body text-center font-bold text-white">
               Communication in Healthcare
-            </h3>
+            </p>
           </div>
           <AnimatePresence>
             {track === 1 && (
@@ -120,11 +121,11 @@ const Graphics = () => {
                 exit={{ opacity: 0 }}
                 className="absolute h-full w-[54vw] top-0 left-[31vw] pl-[5vw] pr-[7vw] flex flex-col justify-center bg-gradient-to-l from-bg to-transparent"
               >
-                <h4 className="text-body-small text-white font-bold">
+                <p className="text-body-small text-white font-bold">
                   Build systems that streamline communication between patients
                   and healthcare providers, such as telemedicine platforms or
                   appointment scheduling apps.
-                </h4>
+                </p>
                 <ul className="hidden md:inline mt-2 text-body-small text-white list-disc list-inside">
                   <li>Improving patient quality of life</li>
                   <li>Improving efficiency of medical workers</li>
@@ -149,9 +150,9 @@ const Graphics = () => {
         >
           <Image src={asteroid_3_cropped} alt="asteroid" />
           <div className="absolute left-0 top-0 size-full flex items-center justify-center">
-            <h3 className="h-fit relative top-[3vw] -left-[2vw] w-1/2 text-body text-center font-bold text-white">
+            <p className="h-fit relative top-[3vw] -left-[2vw] w-1/2 text-body text-center font-bold text-white">
               Communication Platforms for Education
-            </h3>
+            </p>
           </div>
           <AnimatePresence>
             {track === 2 && (
@@ -161,11 +162,11 @@ const Graphics = () => {
                 exit={{ opacity: 0 }}
                 className="absolute h-full w-[52vw] top-0 left-[31vw] pl-[5vw] pr-[7vw] flex flex-col justify-center bg-gradient-to-l from-bg to-transparent"
               >
-                <h4 className="text-body-small text-white font-bold">
+                <p className="text-body-small text-white font-bold">
                   Build specialized platforms that facilitate communication
                   among students, teachers, and parents, enhancing the
                   educational experience.
-                </h4>
+                </p>
                 <ul className="hidden md:block mt-2 text-body-small text-white list-disc list-inside">
                   <li>More accessibility in educational materials</li>
                   <li>Provide a unique learning experience</li>
@@ -187,9 +188,9 @@ const Graphics = () => {
         >
           <Image src={asteroid_4_cropped} alt="asteroid" />
           <div className="absolute left-0 top-0 size-full flex items-center justify-center">
-            <h3 className="h-fit relative top-[3vw] w-1/2 text-body text-center font-bold text-white">
+            <p className="h-fit relative top-[3vw] w-1/2 text-body text-center font-bold text-white">
               Rogue Track
-            </h3>
+            </p>
           </div>
           <AnimatePresence>
             {track === 3 && (
@@ -199,9 +200,9 @@ const Graphics = () => {
                 exit={{ opacity: 0 }}
                 className="absolute h-full w-[60vw] top-0 -left-[60vw] pl-[7vw] pr-[5vw] flex flex-col justify-center bg-gradient-to-r from-bg to-transparent"
               >
-                <h4 className="text-body-small text-white font-bold">
+                <p className="text-body-small text-white font-bold">
                   You Decide!
-                </h4>
+                </p>
                 <ul className="mt-2 text-body-small text-white list-disc list-inside">
                   <li>
                     If the projects do not fit in any of the tracks above, this
