0c46217fedf5ce1b4425ef7751da20e6599dd55b
Ethan Lee <ethan.lee@emory.edu>
2025-02-05 15:13:10 -0500

    Refactor content.jsx to improve semantic structure by changing headings from h1 to h2 and using motion.span for animations

--- src/app/sections/0-landing/content.jsx
+++ src/app/sections/0-landing/content.jsx
@@ -7,41 +7,41 @@ const Content = () => {
     <div className="absolute left-[8vw] top-[120px] sm:top-[14vh] xl:top-[100px] inline-block z-40">
       {/* TITLE */}
       <h1 className="text-[40px] sm:text-[8vw] font-bold leading-none drop-shadow-title">
-        <motion.div
+        <motion.span
           initial={{ opacity: 0, y: -30 }}
           animate={{ opacity: 1.01, y: 0 }}
           transition={{ duration: 1, delay: 2, ease: easeOut }}
           className="block text-gradient"
         >
           EMORY
-        </motion.div>
-        <motion.div
+        </motion.span>
+        <motion.span
           initial={{ opacity: 0, y: -30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 2.3, ease: easeOut }}
           className="block text-gradient"
         >
           HACKS
-        </motion.div>
-        <motion.div
+        </motion.span>
+        <motion.span
           initial={{ opacity: 0, y: -30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 2.6, ease: easeOut }}
           className="block text-gradient"
         >
           2025
-        </motion.div>
+        </motion.span>
       </h1>
 
       {/* DATE */}
-      <motion.h1
+      <motion.h2
         initial={{ opacity: 0, y: -30 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, delay: 3, ease: "easeOut" }}
         className="mt-5 text-[22px] md:text-[3vw] font-bold text-white drop-shadow-title"
       >
         MAR 21 - MAR 23
-      </motion.h1>
+      </motion.h2>
     </div>
   );
 };
