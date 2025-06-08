972a5709d412622518a9e8e43cafbd9efb15e195
Ethan Lee <ethantlee21@gmail.com>
2025-06-08 19:15:38 -0400

    remove unused font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/nav";
 import { fonts } from "@/components/fonts";
