56480487afda0832f4787855c6a18bbd7b597719
Ethan Lee <ethantlee21@gmail.com>
2025-06-15 18:02:32 -0400

    remove base path

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: "export",
-  // images: { unoptimized: true },
-  basePath: "/2026",
 };
 
 export default nextConfig;
