a083c87b2e703cb1bfd3524ac9e8b39b55a4e5d1
Ethan Lee <ethantlee21@gmail.com>
2025-06-15 13:12:45 -0400

    fix: update next.config.ts to change output option and remove images configuration

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  images: { unoptimized: true },
   basePath: "/2026",
 };
 
