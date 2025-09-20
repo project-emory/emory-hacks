1a093d8ead6a7ad98fba7da21627598c03647852
Ethan Lee <ethantlee21@gmail.com>
2025-09-20 18:25:50 -0400

    feat: integrate locomotive scroll for smooth scrolling
    - Implemented locomotive scroll library for smoother scrolling experience.
    - Removed the SmoothScroll component.
    - Added "use client" directive to the page component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
-import SmoothScroll from "@/components/smooth-scroll";
+"use client";
+
 import LandingSection from "@/sections/1-landing";
 import AboutSection from "@/sections/2-about";
 import TracksSection from "@/sections/3-tracks";
 import FaqSection from "@/sections/5-faq";
-import { memo } from "react";
+import { memo, useEffect } from "react";
 
 const Page = memo(() => {
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      new LocomotiveScroll({});
+    })();
+  }, []);
+
   return (
     <main className="pb-40">
-      <SmoothScroll />
       <LandingSection />
       <AboutSection />
       <TracksSection />
