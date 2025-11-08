e77ea94c65a5c685515dc07f6f3dd93a575c2232
Ethan Lee <ethantlee21@gmail.com>
2025-11-08 17:58:35 -0500

    feat: add rosalind mccullough as a speaker and implement dialog for speaker details
    - Added Rosalind McCullough's information to the speakers list.
    - Implemented a dialog component to display speaker details when a speaker is selected.
    - Updated the speakers section to use a grid layout for better responsiveness.

--- src/sections/5-speakers.tsx
+++ src/sections/5-speakers.tsx
@@ -1,6 +1,17 @@
+"use client";
+
 import Section from "@/components/section";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import data from "@/lib/speakers.json";
 import Image from "next/image";
+import { useState } from "react";
 
 const graphics = [{ src: "/graphics/5-speakers/bg.webp", parallaxLevel: 0 }];
 
@@ -11,7 +22,7 @@ const SpeakersSection = () => {
         SPEAKERS
       </h2>
 
-      <div className="flex flex-row lg:flex-wrap my-6 w-full overflow-x-scroll">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-6 my-6 w-full">
         {data.map((speaker, i) => (
           <Speaker key={i} {...speaker} />
         ))}
@@ -29,15 +40,27 @@ type SpeakerProps = {
 };
 
 const Speaker = ({ name, description, img }: SpeakerProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="flex flex-col w-[90%] p-2 max-lg:max-w-100 shrink-0 gap-8 lg:basis-1/2 xl:basis-1/4">
-      <div className="w-full aspect-square shrink-0 grow-0 bg-white/30 rounded-xl overflow-hidden relative">
-        <Image src={img} alt={name} fill className="object-cover" />
-      </div>
-      <div className="space-y-4 text-white">
-        <h3 className="text-lg md:text-xl">{name}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>
+        <div className="hover:-translate-y-2 transition-all">
+          <div className="w-full aspect-square shrink-0 grow-0 bg-white/30 rounded-xl overflow-hidden relative">
+            <Image src={img} alt={name} fill className="object-cover" />
+          </div>
+          <span className="text-white block font-medium mt-2 text-lg md:text-xl">
+            {name}
+          </span>
+        </div>
+      </DialogTrigger>
+
+      <DialogContent className="bg-bg/30 border-white/10 backdrop-blur-xl text-white">
+        <DialogHeader>
+          <DialogTitle>{name}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 };
