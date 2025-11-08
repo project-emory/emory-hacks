89a73076d1b238d688ee40cadc7364670cf26b9a
Ethan Lee <ethantlee21@gmail.com>
2025-11-08 15:28:23 -0500

    feat: implement schedule section
    - Added schedule data from schedule.json
    - Implemented tab switching UI for different days
    - Added animations for schedule items

--- src/sections/4-schedule.tsx
+++ src/sections/4-schedule.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import Section from "@/components/section";
+import data from "@/lib/schedule.json";
+import { useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
 
 const graphics = [
   { src: "/graphics/4-schedule/bg.webp", parallaxLevel: 0 },
@@ -7,17 +12,56 @@ const graphics = [
 ];
 
 const ScheduleSection = () => {
+  const [currDay, setCurrDay] = useState(0);
+  const day = data[currDay];
+
   return (
     <Section graphics={graphics}>
-      <h2 className="text-white font-display text-shadow-2xs text-2xl md:text-6xl xl:text-8xl mb-0 md:mb-4 xl:mb-10 border-shine">
+      <h2 className="text-white font-display text-shadow-2xs text-2xl md:text-6xl xl:text-8xl mb-4 xl:mb-16 border-shine">
         SCHEDULE
       </h2>
 
-      <div className="flex items-center justify-center text-white border-shine text-xl">
-        COMING SOON!
+      <div className="w-full">
+        <div className="text-white text-lg xl:text-2xl font-semibold mb-4 flex gap-4 xl:gap-6">
+          {data.map((d, i) => (
+            <button
+              key={i}
+              data-current={i === currDay}
+              onClick={() => setCurrDay(i)}
+              className="data-[current=true]:border-shine data-[current=false]:opacity-50 transition-all uppercase"
+            >
+              {d.day}
+            </button>
+          ))}
+        </div>
+        <div className="flex flex-col gap-1 md:gap-2 xl:gap-3 bg-bg/30 backdrop-blur-xl rounded-xl border-white p-4">
+          {day.events?.map((item: Event, j) => (
+            <ScheduleItem key={j} {...item} />
+          ))}
+        </div>
       </div>
     </Section>
   );
 };
 
 export default ScheduleSection;
+
+type Event = {
+  time: string;
+  description: string;
+};
+
+const ScheduleItem = (event: Event) => {
+  return (
+    <motion.div
+      key={event.description}
+      initial={{ y: 20, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ ease: "easeOut" }}
+      className="text-white space-x-4 font-medium xl:text-xl"
+    >
+      <span className="font-mono inline-block">{event.time}</span>
+      <span>{event.description}</span>
+    </motion.div>
+  );
+};
